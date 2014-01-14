09427e6cd8ff9db768ea824d9d68530e66b481f5
Christian Genco <christian.genco@gmail.com>
2014-01-13 19:14:13 -0600

    initial commit

---
+++ content/themes/genco/assets/js/index.js
@@ -0,0 +1,27 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+    });
+
+}(jQuery));
+
+
+// google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36669952-1']);
+_gaq.push(['_setDomainName', 'christiangenco.com']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
---
+++ content/themes/genco/todo.md
@@ -0,0 +1,6 @@
+* "featured" will be static
+  * list featured in blog posts too (for now)
+* publish on christian.gen.co
+* add posts and images
+* redirect christian.gen.co to christiangenco.com; set as primary
+* shut down expensive server
